c7400a2a28c5ff929b2b57aa9ec929e3d6880581
vladimirfilip <vlad.filip.self@gmail.com>
2023-08-07 20:21:50 +0100

    Started UI, did orbits simulation page

--- main.py
+++ main.py
@@ -1,49 +1,77 @@
+from typing import Callable
+
+from PyQt6 import QtCore, QtGui, QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+from pages import OrbitsPage, SpirographPage, OrbitsPageSettings, PageClasses, PageIndexes
 import sys
 import matplotlib
 
 matplotlib.use('QtAgg')
 
-from PyQt6 import QtCore, QtGui, QtWidgets
 
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
-from matplotlib.figure import Figure
-from pages import Page1, Page2, Page3, Page4, Page5, Page6, Page7
-
-
-class MplCanvas(FigureCanvasQTAgg):
+class FigureCanvas(FigureCanvasQTAgg):
 
     def __init__(self, parent=None, width=5, height=4, dpi=100):
         fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        super(MplCanvas, self).__init__(fig)
+        self.axes = fig.add_subplot()
+        super(FigureCanvas, self).__init__(fig)
+
+    def set_figure(self, figure: Figure):
+        self.axes = figure.add_subplot()
+
+    def plot(self, *args):
+        self.axes.plot(*args)
 
 
 class MainWindow(QtWidgets.QMainWindow):
+    __TAB_DATA = [
+        (OrbitsPage, "Orbits", "Detailed orbit simulator in 2D and 3D", True),
+        (OrbitsPageSettings, "", "", False),
+        (SpirographPage, "Spirograph", "Spirograph generator", True)
+    ]
 
-    def __init__(self, pages: list, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
+        for i in range(len(MainWindow.__TAB_DATA)):
+            expected_class = PageClasses[PageIndexes(i).name].value
+            assert expected_class == MainWindow.__TAB_DATA[i][0]
         super(MainWindow, self).__init__(*args, **kwargs)
 
-        sc = MplCanvas(self, width=5, height=4, dpi=100)
-        sc.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
-
-        # Create toolbar, passing canvas as first parament, parent (self, the MainWindow) as second.
-        toolbar = NavigationToolbar(sc, self)
-
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(toolbar)
-        layout.addWidget(sc)
-
-        # Create a placeholder widget to hold our toolbar and canvas.
-        widget = QtWidgets.QWidget()
-        widget.setLayout(layout)
+        # sc = MplCanvas(self, width=5, height=4, dpi=100)
+        # sc.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
+        # #
+        # # Create toolbar, passing canvas as first parament, parent (self, the MainWindow) as second.
+        # #
+        # toolbar = NavigationToolbar(sc, self)
+        # layout = QtWidgets.QVBoxLayout()
+        # layout.addWidget(toolbar)
+        # layout.addWidget(sc)
+        # #
+        # # Create a placeholder widget to hold our toolbar and canvas.
+        # #
+        # widget = QtWidgets.QWidget()
+        # widget.setLayout(layout)
         tab_widget = QtWidgets.QTabWidget()
-        tab_widget.addTab(widget, "Demo")
-        for i in range(len(pages)):
-            tab_widget.addTab(pages[i], f"Task {i + 1}")
+        self.central_widget = tab_widget
+        # tab_widget.addTab(widget, "Demo")
+        self.set_tabs(tab_widget)
+        #tab_widget.setTabVisible(2, False)
         self.setCentralWidget(tab_widget)
         self.show()
 
+    def set_tabs(self, tab_widget: QtWidgets.QTabWidget):
+        for i, t in enumerate(MainWindow.__TAB_DATA):
+            contents_class, tab_name, tooltip, is_visible = t
+            tab_widget.addTab(contents_class(self), tab_name)
+            tab_widget.setTabToolTip(i, tooltip)
+            tab_widget.setTabVisible(i, is_visible)
+
+    def switch_to(self, widget_index: int, post_func: Callable = None):
+        self.central_widget.setCurrentIndex(widget_index)
+        if post_func:
+            post_func(self.central_widget)
+
 
 app = QtWidgets.QApplication(sys.argv)
-w = MainWindow([Page1(), Page2(), Page3(), Page4(), Page5(), Page6(), Page7()])
+w = MainWindow()
 app.exec()
--- pages.py
+++ pages.py
@@ -1,46 +1,195 @@
 from PyQt6 import QtCore, QtGui, QtWidgets
+from enum import Enum
+
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+
+PLANETS: list[str] = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
 
 
-class Page1(QtWidgets.QWidget):
-    def __init__(self):
+class PageIndexes(Enum):
+    ORBITS_PAGE = 0
+    ORBITS_PAGE_SETTINGS = 1
+    SPIROGRAPH_PAGE = 2
+
+
+class OrbitSimulationSettings:
+    SETTINGS: dict = {
+        "Centre of orbit": "Sun",
+        "Objects to show": ["Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
+        "Speed": 1,
+        "View type": "2D"
+    }
+
+
+class FigureCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot()
+        super(FigureCanvas, self).__init__(fig)
+
+    def set_figure(self, figure: Figure):
+        self.axes = figure.add_subplot()
+
+    def plot(self, *args):
+        self.axes.plot(*args)
+
+
+class OrbitsPage(QtWidgets.QWidget):
+    ORBITS_STATS: dict[str, QtWidgets.QLabel] = {
+        "Coordinates": None,
+        "Mass": None,
+        "Angular velocity": None,
+        "Linear velocity": None,
+        "Distance from centre": None,
+        "Orbital angle": None,
+        "Eccentricity": None,
+        "Semi-major axis": None,
+        "Semi-minor axis": None,
+        "Orbital period": None,
+    }
+
+    def __init__(self, parent):
         super().__init__()
-        layout = QtWidgets.QGridLayout()
+        self.parent = parent
+        root_layout = QtWidgets.QHBoxLayout()
+        self.sim_settings = OrbitSimulationSettings()
+        self.figure_canvas = FigureCanvas(self)
+        self.figure_canvas.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
+        graph_layout = QtWidgets.QVBoxLayout()
+        toolbar = NavigationToolbar(self.figure_canvas, self)
+        graph_layout.addWidget(toolbar)
+        graph_layout.addWidget(self.figure_canvas)
+        settings_button = QtWidgets.QPushButton("Settings")
+        settings_button.clicked.connect(self.on_settings_button_click)
+        graph_layout.addWidget(settings_button)
+        root_layout.addLayout(graph_layout)
+        #
+        # Creating layout and widgets for user to pick planet to see orbit stats on
+        #
+        planet_picker_layout = QtWidgets.QHBoxLayout()
+        planet_picker_layout.addWidget(QtWidgets.QLabel("Planet: "))
+        planet_picker_dropdown = QtWidgets.QComboBox()
+        planet_picker_dropdown.addItems(PLANETS)
+        planet_picker_dropdown.currentIndexChanged.connect(self.on_planet_dropdown_changed)
+        planet_picker_layout.addWidget(planet_picker_dropdown)
+        #
+        # Creating layout and widgets to display planet orbit stats
+        #
+        for stat_name in OrbitsPage.ORBITS_STATS:
+            OrbitsPage.ORBITS_STATS[stat_name] = QtWidgets.QLabel("N/A")
+        stats_layout = QtWidgets.QGridLayout()
+        for i, orbit_stat in enumerate(OrbitsPage.ORBITS_STATS.keys()):
+            stat_name, stat_value = orbit_stat, OrbitsPage.ORBITS_STATS[orbit_stat]
+            stats_layout.addWidget(QtWidgets.QLabel(stat_name), i // 2 * 2, i % 2, 1, 1)
+            stats_layout.addWidget(stat_value, i // 2 * 2 + 1, i % 2, 1, 1)
+        controls_layout = QtWidgets.QVBoxLayout()
+        controls_layout.addLayout(planet_picker_layout)
+        controls_layout.addLayout(stats_layout)
+        root_layout.addLayout(controls_layout)
+        self.setLayout(root_layout)
 
-        layout.setGeometry(QtCore.QRect(QtCore.QPoint(0, 0), QtCore.QPoint(1100, 700)))
-        label = QtWidgets.QLabel(self)
-        pixmap = QtGui.QPixmap('task1.png')
-        label.setPixmap(pixmap)
-        label.setScaledContents(True)
-        label.setGeometry(0, 0, 1100, 700)
-        layout.addWidget(label, 0, 0)
-        self.setLayout(QtWidgets.QVBoxLayout())
+    @QtCore.pyqtSlot(int)
+    def on_planet_dropdown_changed(self, new_index: int):
+        new_planet: str = PLANETS[new_index]
+        # TODO: add binding to engine to set new stats on the new planet
+        pass
+
+    def on_settings_button_click(self):
+        self.parent.switch_to(PageIndexes.ORBITS_PAGE_SETTINGS.value)
+
+    def update_graph(self):
+        # Called when simulation settings have been updated
+        # TODO: create new figure with new simulation settings
+        pass
+
+    @staticmethod
+    def set_stats(new_stats: dict[str, str]):
+        assert len(new_stats) == len(OrbitsPage.ORBITS_STATS)
+        for k, v in new_stats.items():
+            OrbitsPage.ORBITS_STATS[k].setText(new_stats[k])
 
 
-class Page2(QtWidgets.QWidget):
-    def __init__(self):
+class OrbitsPageSettings(QtWidgets.QWidget):
+    CENTRE_OF_ORBIT_OPTIONS = ["Sun"] + PLANETS
+    OBJECTS_TO_SHOW_OPTIONS = ["Sun"] + PLANETS
+    def __init__(self, parent):
         super().__init__()
+        self.objects_to_show_checkboxes: list[QtWidgets.QCheckBox] = []
+        self.parent = parent
+        self.settings = OrbitSimulationSettings()
+        self.original_settings: dict = self.settings.SETTINGS.copy()
+        root_layout = QtWidgets.QVBoxLayout()
+        button_layout = QtWidgets.QHBoxLayout()
+        reset_button = QtWidgets.QPushButton("Reset")
+        reset_button.clicked.connect(self.on_reset_button_pressed)
+        back_button = QtWidgets.QPushButton("Back")
+        back_button.clicked.connect(self.on_back_button_pressed)
+        button_layout.addWidget(reset_button)
+        button_layout.addWidget(back_button)
+        root_layout.addLayout(button_layout)
+        self.controls_layout = QtWidgets.QVBoxLayout()
+        self.init_settings_widgets()
+        self.set_settings_widget_states()
+        root_layout.addLayout(self.controls_layout)
+        self.setLayout(root_layout)
+
+    def on_reset_button_pressed(self):
+        self.settings.SETTINGS = self.original_settings
+        self.set_settings_widget_states()
+
+    def on_back_button_pressed(self):
+        self.parent.switch_to(PageIndexes.ORBITS_PAGE.value)
+
+    def init_settings_widgets(self):
+        top_half = QtWidgets.QHBoxLayout()
+        centre_of_orbit_layout = QtWidgets.QVBoxLayout()
+        centre_of_orbit_layout.addWidget(QtWidgets.QLabel("Center of orbit"))
+        centre_of_orbit_picker = QtWidgets.QComboBox()
+        centre_of_orbit_picker.addItems(OrbitsPageSettings.CENTRE_OF_ORBIT_OPTIONS)
+        centre_of_orbit_picker.currentIndexChanged.connect(self.on_centre_of_orbit_changed)
+        centre_of_orbit_layout.addWidget(centre_of_orbit_picker)
+        top_half.addLayout(centre_of_orbit_layout)
+        objects_to_show_layout = QtWidgets.QVBoxLayout()
+        objects_to_show_layout.addWidget(QtWidgets.QLabel("Objects to show"))
+        self.objects_to_show_checkboxes = []
+        for i in range(len(OrbitsPageSettings.OBJECTS_TO_SHOW_OPTIONS)):
+            check_box = QtWidgets.QCheckBox(OrbitsPageSettings.OBJECTS_TO_SHOW_OPTIONS[i], self)
+            check_box.stateChanged.connect(lambda: self.on_object_to_show_state_changed(i))
+            objects_to_show_layout.addWidget(check_box)
+            self.objects_to_show_checkboxes.append(check_box)
+        top_half.addLayout(objects_to_show_layout)
+        self.controls_layout.addLayout(top_half)
+
+    def set_settings_widget_states(self):
+        pass
+
+    def on_centre_of_orbit_changed(self, new_index: int):
+        self.settings.SETTINGS["Centre of orbit"] = OrbitsPageSettings.CENTRE_OF_ORBIT_OPTIONS[new_index]
+
+    def on_object_to_show_state_changed(self, index: int):
+        entry = OrbitsPageSettings.OBJECTS_TO_SHOW_OPTIONS[index]
+        is_checked = self.objects_to_show_checkboxes[index].isChecked()
+        if not is_checked:
+            self.settings.SETTINGS["Objects to show"].remove(entry)
+        else:
+            assert entry not in self.settings.SETTINGS
+            self.settings.SETTINGS["Objects to show"].append(entry)
 
 
-class Page3(QtWidgets.QWidget):
-    def __init__(self):
+class SpirographPage(QtWidgets.QWidget):
+    def __init__(self, parent):
         super().__init__()
+        self.parent = parent
+        layout = QtWidgets.QVBoxLayout()
+
+        layout.addWidget(QtWidgets.QLabel("SPIROGRAPH"))
+        self.setLayout(layout)
 
 
-class Page4(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-
-
-class Page5(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-
-
-class Page6(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-
-
-class Page7(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
+class PageClasses(Enum):
+    ORBITS_PAGE = OrbitsPage
+    ORBITS_PAGE_SETTINGS = OrbitsPageSettings
+    SPIROGRAPH_PAGE = SpirographPage
